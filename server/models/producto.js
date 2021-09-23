f58ddc32148faf13b9e9679a009a0e25a29d321b
Daniel Loaiza <danieloaiza@live.com>
2021-09-22 22:38:21 -0600

    Backend

---
+++ server/models/producto.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const ProductoVenta = new mongoose.Schema({
+    tipo:{
+        type:String,
+        required:true,
+    },
+    cantidad:{
+        type:Number,
+        required:true,
+    },
+    precio:{
+        type:Number,
+        required:true,
+    },
+    fecha:{
+        type:String,
+        required:true,
+    },
+    localizacion:{
+        type:String,
+        required:false,
+    },
+    estado:{
+        type:String,
+        required:true,
+    }
+});
+
+
+const EsquemaProducto = new mongoose.Schema({
+    telefono:{
+        type:String,
+        required:true,
+    },
+    publicaciones:{
+        type:[ProductoVenta],
+        required:false,
+    }
+});
+
+const ModeloProducto = mongoose.model('producto',EsquemaProducto);
+module.exports = ModeloProducto; 
